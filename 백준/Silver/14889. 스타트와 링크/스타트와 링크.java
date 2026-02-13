4be7ea12a3554b57f7fe36a2f4eec73d750de4a9
lsh3690 <lshlsh3690@gmail.com>
2026-02-14 07:56:12 +0900

    [Silver I] Title: 스타트와 링크, Time: 296 ms, Memory: 16732 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,74 +1,46 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, depth, ret = Integer.MAX_VALUE;
+	static int N, ret;
+	static int[][] arr;
 
-    static List<Integer> teamA;
-    static List<Integer> teamB;
-    static int[][] arr;
-    static boolean[] tf;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		ret = Integer.MAX_VALUE;
+		for (int mask = 0; mask < 1 << N; mask++) {
+			if (Integer.bitCount(mask) != N / 2)
+				continue;
+			int sumA=0;
+			int sumB=0;
+			for (int i = 0; i < N; i++) {
+				for (int j = i + 1; j < N; j++) {
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        depth = N / 2;
-        tf = new boolean[N];
-        teamA = new ArrayList<>();
-        teamB = new ArrayList<>();
+					if ((mask & (1 << i)) != 0 && (mask & (1 << j)) != 0) {
+						// 둘 다 A팀
+						sumA += arr[i][j] + arr[j][i];
+					}
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+					else if ((mask & (1 << i)) == 0 && (mask & (1 << j)) == 0) {
+						// 둘 다 B팀
+						sumB += arr[i][j] + arr[j][i];
+					}
+				}
+			}
+			ret = Math.min(ret, Math.abs(sumA - sumB));
 
+		}
 
-        boj14889_dfs(0, 0);
-        System.out.println(ret);
-    }
-
-    private static void boj14889_dfs(int idx, int cnt) {
-        if (cnt == depth) {
-            int sumA =0;
-            int sumB=0;
-
-            for (int i = 0; i < N; i++) {
-                if (tf[i]) {
-                    for (int j = i+1; j < N; j++) {
-                        if (tf[j]) {
-                            sumA += arr[i][j] + arr[j][i];
-                        }
-                    }
-
-                } else if (!tf[i]) {
-                    for (int j = i+1; j < N; j++) {
-                        if (!tf[j]) {
-                            sumB += arr[i][j] + arr[j][i];
-                        }
-                    }
-                }
-            }
-
-
-            ret=Math.min(ret, Math.abs(sumA-sumB));
-
-
-            teamA.clear();
-            teamB.clear();
-            return;
-        }
-        for (int i = idx; i < N; i++) {
-            if (!tf[i]) {
-                tf[i] = true;
-                boj14889_dfs(i+1, cnt+1);
-                tf[i] = false;
-            }
-        }
-    }
+		System.out.println(ret);
+	}
 }
