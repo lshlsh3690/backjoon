82d12f45770137361300ed6929f673b1a75b3e3d
lsh3690 <lshlsh3690@gmail.com>
2026-02-24 15:03:15 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 907 ms, Memory: 95,840 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -1,112 +1,180 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-    private static int N, H, W, T, result;
-    private static int[] dy = {-1, 0, 1, 0};
-    private static int[] dx = {0, 1, 0, -1};
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
+	
+	static int T, N, W, H;
+	static int ans ;
+	static int[][] arr;
+	static int[][] bombedArr;
+	static int[][] gravityArr;
+	static boolean[][] visited; 
+	
+	static ArrayDeque<int[]> que = new ArrayDeque<>();
+	static int[] selected;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	
+	 public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 T = Integer.parseInt(br.readLine());
+		 
+		 for (int tc = 1 ; tc <= T ; tc ++) {
+			 StringTokenizer st = new StringTokenizer(br.readLine());
+			 N = Integer.parseInt(st.nextToken());
+			 W = Integer.parseInt(st.nextToken());
+			 H = Integer.parseInt(st.nextToken());
+			 ans = Integer.MAX_VALUE;
+			 arr = new int[H][W];
+			 bombedArr = new int[H][W];
+			 gravityArr = new int[H][W];
+			 visited = new boolean[H][W];
+			 selected = new int[N];
+			 que.clear();
+			 
+			 for (int i = 0 ; i < H ; i ++) {
+				 st = new StringTokenizer(br.readLine());
+				 for (int j = 0 ; j < W ; j ++) {
+					 arr[i][j] = Integer.parseInt(st.nextToken());
+				 }
+			 }
 
-        for (int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            N = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            H = Integer.parseInt(st.nextToken());
-            int[][] arr = new int[H][W];
-            result = Integer.MAX_VALUE;
-            for (int i = 0; i < H; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                for (int j = 0; j < W; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
+			 selectBombColOrder(0); // 폭탄을 떨어트릴 열의 순서를 중복 순열로 구한다 O(W^N)
+			 
+//			 initBombedArr();
+//			 
+//			 bomb(2);
+//			 initGravityArr();
+//			 gravity();
+//			 
+//			 bomb(2);
+//			 initGravityArr();
+//			 gravity();
+//			 
+//			 bomb(6);
+//			 initGravityArr();
+//			 gravity();
+//			 
+//			 System.out.println("============================");
+//			 for (int[] a : bombedArr) {
+//				 System.out.println(Arrays.toString(a));
+//			 }
 
-            for (int idx = 0; idx < W; idx++) {
-                dfs(idx, 0, arr);
-            }
+			 System.out.println("#" + tc + " " + ans);
+		 }
+	}
+	 
+	 static void selectBombColOrder(int idx) {
+		 if(idx == N) {
+			 // 폭탄 터트릴 arr 배열 복사, visited 배열 초기화 
+			 initBombedArr();
+			 initVisited();
+			 
+			 for(int col : selected) {
+				 bomb(col);
+				 initGravityArr();
+				 gravity();
+			 }
+			 
+			 ans = Math.min(ans, cntBlock());
+			 
+			 
+			 return;
+		 }
+		 
+		 for (int i = 0 ; i < W ; i ++) {
+			 selected[idx] = i;
+			 selectBombColOrder(idx + 1);
+		 }
+	 }
+	 
+	 static void bomb(int col) {
+		 // 해당 열에서 가장 처음 만나는 0이 아닌 원소를 que에 넣는다.
+		 // que가 빌 떄 까지 que에 있는 값을 터트리고, 연쇄 작용될 곳을 que에 넣는다.
+		 
+		 for (int i = 0 ; i < H ; i ++) {
+			 if(bombedArr[i][col] != 0) {
+				 que.add(new int[] {i, col, bombedArr[i][col]});
+				 bombedArr[i][col] = 0;
+				 break;
+			 }
+		 }
+		 
+		 while(!que.isEmpty()) {
+			 int[] cur = que.poll();
+			 int x = cur[0];
+			 int y = cur[1];
+			 int dist = cur[2];
+			 
+			 
+			 for (int i = 0 ; i < 4 ; i ++) {
+				 for (int d = 0 ; d < dist ; d ++) {
+					 int nx = x + dx[i] * d;
+					 int ny = y + dy[i] * d;
+					 
+					 if(nx < 0 || nx >= H || ny < 0 || ny >= W) continue;
+					 if(bombedArr[nx][ny] != 0) {
+						 que.add(new int[] {nx, ny, bombedArr[nx][ny]});
+						 bombedArr[nx][ny] = 0;
+					 }
+				 }
+			 }
+			 
+		 }
+	 }
+	 
+	 static void gravity() {
+		 for (int j = 0 ; j < W ; j ++) {
+			 int col = H - 1;
+			 for (int i = H - 1 ; i >= 0 ; i --) {
+				 if(bombedArr[i][j] != 0) {
+					 gravityArr[col --][j] = bombedArr[i][j];
+				 }
+			 }
+		 }
+		 
+		 for (int i = 0 ; i < H ; i ++) {
+			 for (int j = 0 ; j < W ; j ++) {
+				 bombedArr[i][j] = gravityArr[i][j];
+			 }
+		 }
+	 }
+	 
+	 static int cntBlock() {
+		 int cnt = 0;
+		 
+		 for (int i = 0 ; i < H ; i ++) {
+			 for (int j = 0 ; j < W ; j ++) {
+				 if (bombedArr[i][j] != 0) ++ cnt;
+			 }
+		 }
+		 
+		 return cnt;
+	 }
+	 
+	 static void initBombedArr() {
+		 for (int i = 0 ; i < H ; i ++) {
+			 for (int j = 0 ; j < W ; j ++) {
+				 bombedArr[i][j] = arr[i][j];
+			 }
+		 }
+	 }
+	 
+	 
+	 static void initGravityArr() {
+		 for (int i = 0 ; i < H ; i ++) {
+			 for (int j = 0 ; j < W ; j ++) {
+				 gravityArr[i][j] = 0 ;
+			 }
+		 }
+	 }
 
-            System.out.println("#" +t+ " " +result);
-        }
-    }
-
-    private static void dfs(int idx, int depth, int[][] arr) {
-        if (depth == N) {
-            int sum = 0;
-            for (int i = 0; i < H; i++) {
-                for (int j = 0; j < W; j++) {
-                    if (arr[i][j] > 0) {
-                        sum++;
-                    }
-                }
-            }
-            result = Math.min(sum, result);
-
-            return;
-        }
-        int[][] clone = new int[H][W];
-        for (int i = 0; i < H; i++) {
-            clone[i] = arr[i].clone();
-        }
-
-        int height = 0;
-        while (clone[height][idx] == 0) {
-            height++;
-            if (height == H-1) {
-                break;
-            }
-        }
-
-        if (clone[height][idx] == 1) {
-            clone[height][idx] = 0;
-        }else{
-            Queue<int[]> q = new LinkedList<>();
-            q.add(new int[]{height, idx});
-            while (!q.isEmpty()) {
-                int[] pos = q.poll();
-                int y = pos[0];
-                int x = pos[1];
-                int distance = clone[y][x];
-                clone[y][x] = 0;
-                for (int d = 1; d<distance;d++){
-                    for (int k = 0; k < 4; k++) {
-                        int ny = y + (dy[k]*d);
-                        int nx = x + (dx[k]*d);
-                        if (ny < 0 || ny >= H || nx < 0 || nx >= W || clone[ny][nx] == 0) {
-                            continue;
-                        }
-
-                        if (clone[ny][nx] == 1) {
-                            clone[ny][nx] = 0;
-                            continue;
-                        }
-                        if (clone[ny][nx] != 1) {
-                            q.add(new int[]{ny, nx});
-                        }
-                    }
-                }
-            }
-            for (int w = 0; w < W; w++) {
-                for (int h = H-1; h > 0; h--) {
-                    if (clone[h][w] == 0) {
-                        int tmpH = h - 1;
-                        while (clone[tmpH][w] == 0 && tmpH>0) {
-                            tmpH--;
-                        }
-                        clone[h][w] = clone[tmpH][w];
-                        clone[tmpH][w] = 0;
-                    }
-                }
-            }
-        }
-
-        for (int i = 0; i < W; i++) {
-            dfs(i, depth + 1, clone);
-        }
-
-    }
+	 
+	 static void initVisited() {
+		 for (int i = 0 ; i < H ; i ++) {
+			 for (int j = 0 ; j < W ; j ++) {
+				 visited[i][j] = false;
+			 }
+		 }
+	 }
 }
